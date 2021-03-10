caf91a6be83bcd47eb05b4b95622c2101c01c442
John Doe <T6nu.koll@gmail.com>
2021-03-11 00:34:37 +0200

    Add bitcoin calculator

---
+++ BitCoinApp_TõnuKoll/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace BitCoinApp_TõnuKoll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.currencyCombo = new System.Windows.Forms.ComboBox();
+            this.btnGetRates = new System.Windows.Forms.Button();
+            this.AmountOfTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // currencyCombo
+            // 
+            this.currencyCombo.FormattingEnabled = true;
+            this.currencyCombo.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "GBP"});
+            this.currencyCombo.Location = new System.Drawing.Point(197, 75);
+            this.currencyCombo.Name = "currencyCombo";
+            this.currencyCombo.Size = new System.Drawing.Size(121, 23);
+            this.currencyCombo.TabIndex = 0;
+            this.currencyCombo.Text = "Select currency";
+            // 
+            // btnGetRates
+            // 
+            this.btnGetRates.Location = new System.Drawing.Point(376, 75);
+            this.btnGetRates.Name = "btnGetRates";
+            this.btnGetRates.Size = new System.Drawing.Size(75, 23);
+            this.btnGetRates.TabIndex = 1;
+            this.btnGetRates.Text = "Get rates";
+            this.btnGetRates.UseVisualStyleBackColor = true;
+            this.btnGetRates.Click += new System.EventHandler(this.btnGetRates_Click);
+            // 
+            // AmountOfTextBox
+            // 
+            this.AmountOfTextBox.Location = new System.Drawing.Point(197, 149);
+            this.AmountOfTextBox.Name = "AmountOfTextBox";
+            this.AmountOfTextBox.Size = new System.Drawing.Size(101, 21);
+            this.AmountOfTextBox.TabIndex = 2;
+            this.AmountOfTextBox.TextChanged += new System.EventHandler(this.AmountOfTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount of bitcoins";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.BackColor = System.Drawing.Color.Transparent;
+            this.resultLabel.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(197, 183);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(36, 15);
+            this.resultLabel.TabIndex = 4;
+            this.resultLabel.Text = "Result";
+            this.resultLabel.Visible = false;
+            this.resultLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(200, 201);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(100, 21);
+            this.ResultTextBox.TabIndex = 5;
+            this.ResultTextBox.Visible = false;
+            this.ResultTextBox.TextChanged += new System.EventHandler(this.ResultTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1065, 548);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AmountOfTextBox);
+            this.Controls.Add(this.btnGetRates);
+            this.Controls.Add(this.currencyCombo);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Bitcoin Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox currencyCombo;
+        private System.Windows.Forms.Button btnGetRates;
+        private System.Windows.Forms.TextBox AmountOfTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox ResultTextBox;
+    }
+}
+
---
+++ BitCoinApp_TõnuKoll/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitCoinApp_TõnuKoll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetRates_Click(object sender, EventArgs e)
+        {
+            if (currencyCombo.SelectedItem.ToString()=="EUR") //kui valib euro
+            {
+                if (AmountOfTextBox.Text == string.Empty)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(AmountOfTextBox.Text), "Amount of bitcoins not entered!");
+                }
+                resultLabel.Visible = true;
+                ResultTextBox.Visible = true;
+                BitcoinRates bitcoin = GetRates();
+                float result = Int32.Parse(AmountOfTextBox.Text) * bitcoin.bpi.EUR.rate_float;
+                ResultTextBox.Text = $"{result.ToString()} {bitcoin.bpi.EUR.code}";
+            }
+
+            if (currencyCombo.SelectedItem.ToString() == "USD") //kui valib USD
+            {
+                if (AmountOfTextBox.Text == string.Empty)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(AmountOfTextBox.Text), "Amount of bitcoins not entered!");
+                }
+                resultLabel.Visible = true;
+                ResultTextBox.Visible = true;
+                BitcoinRates bitcoin = GetRates();
+                float result = Int32.Parse(AmountOfTextBox.Text) * bitcoin.bpi.USD.rate_float;
+                ResultTextBox.Text = $"{result.ToString()} {bitcoin.bpi.USD.code}";
+            }
+
+            if (currencyCombo.SelectedItem.ToString() == "GBP") //kui valib GBP
+            {
+                if (AmountOfTextBox.Text == string.Empty)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(AmountOfTextBox.Text), "Amount of bitcoins not entered!");
+                }
+                resultLabel.Visible = true;
+                ResultTextBox.Visible = true;
+                BitcoinRates bitcoin = GetRates();
+                float result = Int32.Parse(AmountOfTextBox.Text) * bitcoin.bpi.GBP.rate_float;
+                ResultTextBox.Text = $"{result.ToString()} {bitcoin.bpi.GBP.code}";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResultTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static BitcoinRates GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitcoinRates bitcoin;
+            using(var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<BitcoinRates>(response);
+            }
+
+            return bitcoin;
+        }
+
+        private void AmountOfTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
